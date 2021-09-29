9fe0f0efffcdcf5f3fd236940d748e8ac1abd435
Daniel Calvin <arockne2003@yahoo.com>
2021-09-29 13:01:56 -0700

    commit tree ef570eedcd5383320edc4f66fa8791cb6a18b066

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+#define method parrot
+  #accepts string
+  #default param "Squawk!"
+def parrot(str = "Squawk!")
+  puts str
+  str
+end
